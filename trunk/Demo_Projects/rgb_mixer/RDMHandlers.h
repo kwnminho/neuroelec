f0ec631d305be800bd1930c566818c8a3b1bf89a
neurostar@gmail.com <neurostar@gmail.com@86ab0a7a-ca30-c4f9-fdc8-837f00208d51>
2012-03-05 20:58:07 +0000

    rgb_mixer commit

---
+++ trunk/Demo_Projects/rgb_mixer/RDMHandlers.h
@@ -0,0 +1,41 @@
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * RDMHandlers.h
+ * Copyright (C) 2011 Simon Newton
+ */
+
+
+#ifndef RDM_HANDLERS_H
+#define RDM_HANDLERS_H
+
+#include "WProgram.h"
+
+
+/**
+ * Setup the RDM Handling
+ */
+void SetupRDMHandling();
+
+/*
+ * Handle an RDM message
+ * @param message pointer to a RDM message where the first byte is the sub star
+ * code.
+ * @param size the size of the message data.
+ */
+void HandleRDMMessage(const byte *message, int size);
+
+
+#endif  // RDM_HANDLERS_H
